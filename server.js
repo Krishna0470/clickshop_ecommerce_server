6f85c4cb9d877c06f09a5e93c36b19ee762ce5e0
krishna <krishnasabu885@gmail.com>
2024-02-28 17:26:59 +0530

    logincommit

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 const express =require('express');
 const app = express();
 let dotevn = require('dotenv');
+const cors = require('cors')
 const authRoutes = require('./routes/authRoutes');
 const  connect  = require('./db/config/config');
 dotevn.config();
 
+
+
+
 const port = process.env.PORT
+
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended:false}))
+app.use(authRoutes);
+
 
 app.get('/',(req,res)=>{
     res.status(200).send('Test API')
 });
-app.use(express.json());
-app.use(authRoutes)
 
-connect();
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-});
+connect()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on http://localhost:${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error('Error connecting to the database:', err);
+    });
